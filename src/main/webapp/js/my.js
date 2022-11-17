22780277caaae451a162ac7d2de81a205756ada1
bulo4kasmyasom <benetoru@gmail.com>
2022-11-17 20:19:58 +0300

    working project

--- src/main/webapp/js/my.js
+++ src/main/webapp/js/my.js
@@ -1,65 +1,96 @@
-const urlPlayers = '/rest/players'
-const urlCountPlayers = urlPlayers + '/count'
+// Notice for me.
+// Can be added/improved to the project:
+// - History API
+// - Cookies for page size, page number and editNow
+// - Multiple deletion of players
+// - Separate the JS code and files
+// - function updatePaginator() can be improved. If too much pages:
+// Example:  [first_Page] 3 4 [current_page] .. 8 9 10 [last_Page]
 
-const imgDeletePath = "/img/delete.png";
-const imgEditPath = "/img/edit.png";
-const imgSavePath = "/img/save.png";
+//But this is not a task of the project...
 
-// encoding troubles...
-// const NAMES = ["Эндарион", "Морвиел", "Трениган", "Балгор", "Джерис", "Иллинас", "Ардонг", "Эззэссэль", "Айша", "Дмитрий"]
-// const TITLES = ["Смертоносный", "Искусительница", "Серый Воин", "Имперский Воин", "Приходящий Без Шума", "Ухастый Летун"]
+const PLAYERS_PATH = '/rest/players';
+const COUNT_PLAYERS_PATH = PLAYERS_PATH + '/count';
+const IMG_DELETE_PATH = "/img/delete.png";
+const IMG_EDIT_PATH = "/img/edit.png";
+const IMG_SAVE_PATH = "/img/save.png";
+
+const PLAYER_ADDED_SHOW_HIDE_TIMEOUT = 3000;
+const checkInputFieldByEmpty = "Empty characters are introduced, check input field: ";
+const NOT_FOUND_PLAYERS = "Not found players";
+const BAD_REQUEST = "bad request";
+const BAD_PARAMETERS = "Bad parameters...";
+const PLAYER_WAS_ADDED = "Player was added";
+const SO_FAST = "Try to create a new player " + PLAYER_ADDED_SHOW_HIDE_TIMEOUT + " milliseconds after adding the past player";
+
 const NAMES = ["Endarion", "Morviel", "Training", "Balgor", "Jeris", "Illynas", "Ardong", "Ezzessel", "Aisha", "Dmitry"]
 const TITLES = ["Deadly", "temptic", "gray warrior", "imperial warrior", "coming without noise", "brittle flying"]
 const RACES = ["HUMAN", "DWARF", "ELF", "GIANT", "ORC", "TROLL", "HOBBIT"]
 const PROFESSIONS = ["WARRIOR", "ROGUE", "SORCERER", "CLERIC", "PALADIN", "NAZGUL", "WARLOCK", "DRUID"]
 const BANNED = ["false", "true"]
 
-// можно заменить на cookies
-let PAGE_SIZE = 7
-let PAGE_NUMBER = 0
+const GET_TABLE_ID = getQSelector("#tableBody");
+const GET_PAGES_ID = getQSelector("#paginator");
+const CREATE_PLAYER_FORM_ID = getQSelector("#createPlayerForm");
+const INPUT_NAME_ID = getQSelector("#inputName");
+const INPUT_TITLE_ID = getQSelector("#inputTitle");
+const INPUT_LEVEL_ID = getQSelector("#inputLevel");
+const SET_PROFESSION_ID = getQSelector("#selectProfession");
+const SELECT_BANNED_ID = getQSelector("#selectBanned");
+const SELECT_RACE_ID = getQSelector("#selectRace");
+const PLAYER_ADDED_RESULT_ID = getQSelector("#playerAddedResult");
+
+let player = null;
+let idFieldsCreateNewPlayer = new Map();
+let arrLevels = []
+let page_size = 7
+let page_number = 0
 let editNow = false
 
-const getTableId = getQSelector("#tableBody")
-const getPagesId = getQSelector("#paginator")
-const createPlayerFormId = getQSelector("#createPlayerForm");
-
-const inputNameId = getQSelector("#inputName");
-const inputTitleId = getQSelector("#inputTitle");
-const inputLevelId = getQSelector("#inputLevel");
-const selectProfessionId = getQSelector("#selectProfession");
-const selectBannedId = getQSelector("#selectBanned");
-const selectRaceId = getQSelector("#selectRace");
+idFieldsCreateNewPlayer.set(INPUT_NAME_ID, NAMES)
+idFieldsCreateNewPlayer.set(INPUT_TITLE_ID, TITLES)
+idFieldsCreateNewPlayer.set(INPUT_LEVEL_ID, arrLevels)
+idFieldsCreateNewPlayer.set(SET_PROFESSION_ID, PROFESSIONS)
+idFieldsCreateNewPlayer.set(SELECT_BANNED_ID, BANNED)
+idFieldsCreateNewPlayer.set(SELECT_RACE_ID, RACES)
 
 const actions = {
     edit: {
-        imgUrl: imgEditPath,
+        imgUrl: IMG_EDIT_PATH,
         actionText: "edit",
         title: "Edit",
         alt: "Edit",
-        action: (id) => editPlayer(id)
+        action: (id) => player.editPlayer(id)
     },
     delete: {
-        imgUrl: imgDeletePath,
+        imgUrl: IMG_DELETE_PATH,
         actionText: "delete",
         title: "Delete",
         alt: "Delete",
-        action: (id) => deletePlayer(id)
+        action: (id) => player.deletePlayer(id)
     }
 }
 
 window.onload = function () {
-    sendRequest('GET', urlPlayers + '?pageSize=' + PAGE_SIZE)
+    onLoadWindow();
+}
+
+function onLoadWindow() {
+    player = new Player()
+
+    sendRequest('GET', getUrlWithParamsPageSize())
         .then(players => createTable(players))
         .catch(error => console.log(error))
-    updatePaginator(PAGE_SIZE)
 
-    addToSelectOption(selectBannedId, BANNED);
-    addToSelectOption(selectRaceId, RACES);
-    addToSelectOption(selectProfessionId, PROFESSIONS);
+    updatePaginator(page_size)
 
-    createPlayerFormId.addEventListener("submit", function (event) {
+    addToSelectOption(SELECT_BANNED_ID, BANNED);
+    addToSelectOption(SELECT_RACE_ID, RACES);
+    addToSelectOption(SET_PROFESSION_ID, PROFESSIONS);
+
+    CREATE_PLAYER_FORM_ID.addEventListener("submit", function (event) {
         event.preventDefault();
-        createPlayer()
+        player.createPlayer()
     }, true);
 }
 
@@ -79,7 +110,9 @@ function addToSelectOption(qSelector, array) {
 }
 
 function showHideElement(id) {
-    let element = document.getElementById(id);
+    let element = id;
+    if (typeof id === 'string')
+        element = getQSelector(id);
     let display = element.style.display;
     if (display !== "block")
         element.style.display = "block";
@@ -94,10 +127,10 @@ function sendRequest(method, url, params = null) {
         xhr.responseType = 'json'
         xhr.setRequestHeader('Content-Type', 'application/json')
         xhr.onload = () => {
-            if (xhr.status >= 400)
-                reject(xhr.response);
-            else
-                resolve(xhr.response);
+            if (xhr.status === 400 || xhr.status === 404) {
+                window.location = "/html/" + xhr.status + ".html" // only for this project
+            }
+            resolve(xhr.response);
         }
         xhr.onerror = () => {
             reject(xhr.response)
@@ -121,12 +154,12 @@ function replaceDataTd(qSid, type) {
     return create
 }
 
-function getPlayersWithParams(pageSize, pageNumber) {
-    sendRequest('GET', urlPlayers + "?pageSize=" + pageSize + '&pageNumber=' + pageNumber)
+function getPlayersWithParams() {
+    sendRequest('GET', getUrlWithParamsPageSizeNumber())
         .then(players => {
             updateTable(players)
         })
-        .catch(error => console.log(error));
+        .catch(() => console.log(NOT_FOUND_PLAYERS));
 }
 
 function createInputField(qSelector, id, type) {
@@ -171,15 +204,11 @@ function removeAllChilds(elem) {
 }
 
 function pasteRandomValueInIFields() {
-    let arrLevels = []
     for (let i = 0; i <= 100; i++) arrLevels.push(i)
-    // можно заменить на мапу и циклом гонять
-    getRandomValue(inputNameId, NAMES)
-    getRandomValue(inputTitleId, TITLES)
-    getRandomValue(inputLevelId, arrLevels)
-    getRandomValue(selectProfessionId, PROFESSIONS)
-    getRandomValue(selectBannedId, BANNED)
-    getRandomValue(selectRaceId, RACES)
+
+    idFieldsCreateNewPlayer.forEach((array, id) => {
+        getRandomValue(id, array);
+    });
 }
 
 function createTable(data) {
@@ -198,7 +227,7 @@ function createTable(data) {
                 addActionsInTable(person, row);
             }
         }
-        getTableId.appendChild(row);
+        GET_TABLE_ID.appendChild(row);
     }
 }
 
@@ -218,117 +247,165 @@ function addActionsInTable(person, row) {
     }
 }
 
-function deletePlayer(id) {
-    sendRequest('DELETE', urlPlayers + '/' + id)
-        .then(() => {
-            sendRequest('GET', urlPlayers + "?pageNumber=" + PAGE_NUMBER + "&pageSize=" + PAGE_SIZE)
-                .then(players => {
-                    if (Object.keys(players).length > 0)
-                        updateTable(players)
-                    else
-                        location.reload(); //
-                })
-                .catch(error => console.log(error));
+function updateTable(players) {
+    removeAllChilds(GET_TABLE_ID);
+    createTable(players)
+    updatePaginator(page_size)
+    editNow = false
+}
+
+function selectPageSizeForView(pageSize) {
+    page_number = 0
+    page_size = pageSize
+    sendRequest('GET', getUrlWithParamsPageSize())
+        .then(players => {
+            updateTable(players)
         })
-        .catch(error => console.log(error))
+        .catch(() => console.log(NOT_FOUND_PLAYERS));
 }
 
-function editPlayer(id) {
-    if (editNow) return false;
+function updatePaginator(playersCountInTableNow = page_size) {
+    removeAllChilds(GET_PAGES_ID)
 
-    editNow = true
-    let editImgId = getQSelector("#edit-" + id);
-    editImgId.setAttribute("src", imgSavePath)
-    editImgId.setAttribute("id", "save-" + id)
-    let deleteId = getQSelector("#delete-" + id);
-    deleteId.style.display = 'none'
-
-    let editFieldName = replaceDataTd("#name-" + id, 'text')
-    let editFieldTitle = replaceDataTd("#title-" + id, 'text')
-    let editFieldRace = replaceDataTd("#race-" + id, RACES)
-    let editFieldProfession = replaceDataTd("#profession-" + id, PROFESSIONS)
-    let editFieldBanned = replaceDataTd("#banned-" + id, BANNED)
-
-    editImgId.addEventListener("click", () => {
-        let dataParams = {
-            name: editFieldName.value.trim().slice(0, 12),
-            title: editFieldTitle.value.trim().slice(0, 30),
-            race: editFieldRace.value,
-            profession: editFieldProfession.value,
-            banned: editFieldBanned.value,
-        }
-        sendRequest('POST', urlPlayers + '/' + id, dataParams)
-            .then(() => {
-                getPlayersWithParams(PAGE_SIZE, PAGE_NUMBER)
-                editNow = false
-            })
-            .catch(error => console.log(error))
-    }, false)
-}
-
-function createPlayer() {
-    let formData = new FormData(createPlayerFormId);
-    let dataParams = {}
-
-    for (let [name, value] of formData) {
-        if (!isEmpty(value.trim()))
-            dataParams[name] = value; // нужно делать проверку входных данных или проверять их на сервере
-        else
-            return alert("Empty characters are introduced, check input field: " + name)
-
-        if (name === 'birthday')
-            dataParams[name] = Date.parse(value.toString())
-    }
-    sendRequest('POST', urlPlayers, dataParams)
-        .then(() => {
-            getPlayersWithParams(PAGE_SIZE, PAGE_NUMBER)
-            alert("Player was added")
-            pasteRandomValueInIFields();
-        })
-        .catch(() => alert("Bad parameters..."))
-    updatePaginator(PAGE_SIZE)
-}
-
-function updatePaginator(playersCountInTableNow = PAGE_SIZE) {
-    removeAllChilds(getPagesId)
-
-    sendRequest('GET', urlCountPlayers)
+    sendRequest('GET', COUNT_PLAYERS_PATH)
         .then(allPlayersCount => {
             let countPagesInPaginator = Math.ceil(allPlayersCount / playersCountInTableNow);
 
             for (let pageNumber = 0; pageNumber < countPagesInPaginator; pageNumber++) {
                 let link = document.createElement("span");
                 link.addEventListener("click", () => {
-                    getPlayersWithParams(playersCountInTableNow, pageNumber)
-                    PAGE_NUMBER = pageNumber
-                    PAGE_SIZE = playersCountInTableNow
+                    page_number = pageNumber
+                    page_size = playersCountInTableNow
+                    getPlayersWithParams()
                 }, false)
 
-                if (PAGE_NUMBER === pageNumber)
+                if (page_number === pageNumber)
                     link.className = 'activePageNumber'
                 else
-                    link.className = "pageNumber"
+                    link.className = 'pageNumber'
 
                 link.textContent = pageNumber.toString()
-                getPagesId.appendChild(link)
+                GET_PAGES_ID.appendChild(link)
             }
         })
-        .catch(error => console.log(error));
+        .catch(() => console.log(NOT_FOUND_PLAYERS));
 }
 
-function updateTable(players) {
-    removeAllChilds(getTableId);
-    createTable(players)
-    updatePaginator(PAGE_SIZE)
-    editNow = false
+function getUrlWithParamsPageSize() {
+    return PLAYERS_PATH + "?pageSize=" + page_size;
 }
 
-function selectPageSizeForView(pageSize) {
-    sendRequest('GET', urlPlayers + "?pageSize=" + pageSize)
-        .then(players => {
-            PAGE_NUMBER = 0
-            PAGE_SIZE = pageSize
-            updateTable(players)
-        })
-        .catch(error => console.log(error));
+function getUrlWithParamsPageSizeNumber() {
+    return PLAYERS_PATH + "?pageNumber=" + page_number + "&pageSize=" + page_size;
+}
+
+
+class Player {
+    tooFastCreateNewPlayer = false
+
+    createPlayer() {
+        let formData = new FormData(CREATE_PLAYER_FORM_ID)
+        let dataParams = {}
+
+        for (let [name, value] of formData) {
+            if (!isEmpty(value.trim()))
+                dataParams[name] = value; // need to check params in server or with js or redirect 400 page
+            else
+                return alert(checkInputFieldByEmpty + name)
+
+            if (name === 'birthday')
+                dataParams[name] = Date.parse(value.toString())
+        }
+        if (!this.tooFastCreateNewPlayer) {
+            this.tooFastCreateNewPlayer = true
+            sendRequest('POST', PLAYERS_PATH, dataParams)
+                .then(response => {
+                    getPlayersWithParams()
+                    this.playerWasAddedResult(response);
+                    pasteRandomValueInIFields();
+                })
+                .catch(() => alert(BAD_PARAMETERS))
+            updatePaginator(page_size)
+        } else {
+            alert(SO_FAST)
+        }
+    }
+
+    playerWasAddedResult(response) {
+        let resultElement = document.createElement("span")
+
+        resultElement.innerHTML = "<b>" + PLAYER_WAS_ADDED +
+            "<br> Name:</b> " + response.name +
+            "<br> <b>Title:</b> " + response.title
+
+        removeAllChilds(PLAYER_ADDED_RESULT_ID)
+        PLAYER_ADDED_RESULT_ID.appendChild(resultElement)
+        showHideElement(PLAYER_ADDED_RESULT_ID)
+
+        setTimeout(() => {
+            showHideElement(PLAYER_ADDED_RESULT_ID)
+            this.tooFastCreateNewPlayer = false
+        }, PLAYER_ADDED_SHOW_HIDE_TIMEOUT)
+    }
+
+
+    editPlayer(id) {
+        if (editNow) return false;
+        editNow = true
+        let editImgId = getQSelector("#edit-" + id);
+        let deleteId = getQSelector("#delete-" + id);
+
+        editImgId.setAttribute("src", IMG_SAVE_PATH)
+        editImgId.setAttribute("id", "save-" + id)
+        deleteId.style.display = 'none'
+
+        let editFieldName = replaceDataTd("#name-" + id, 'text')
+        let editFieldTitle = replaceDataTd("#title-" + id, 'text')
+        let editFieldRace = replaceDataTd("#race-" + id, RACES)
+        let editFieldProfession = replaceDataTd("#profession-" + id, PROFESSIONS)
+        let editFieldBanned = replaceDataTd("#banned-" + id, BANNED)
+
+        this.imageEventListener(editImgId, editFieldName, editFieldTitle, editFieldRace, editFieldProfession, editFieldBanned, id);
+    }
+
+    imageEventListener(editImgId, editFieldName, editFieldTitle, editFieldRace, editFieldProfession, editFieldBanned, id) {
+        editImgId.addEventListener("click", () => {
+            let dataParams = {
+                name: editFieldName.value.trim().slice(0, 12),
+                title: editFieldTitle.value.trim().slice(0, 30),
+                race: editFieldRace.value,
+                profession: editFieldProfession.value,
+                banned: editFieldBanned.value,
+            }
+
+            for (const key in dataParams) {
+                if (isEmpty(`${dataParams[key]}`.trim()))
+                    return alert(checkInputFieldByEmpty + `${key}`)
+            }
+
+            sendRequest('POST', PLAYERS_PATH + '/' + id, dataParams)
+                .then(() => {
+                    getPlayersWithParams()
+                    editNow = false
+                })
+                .catch(() => console.log(BAD_REQUEST))
+        }, false)
+    }
+
+
+    deletePlayer(id) {
+        sendRequest('DELETE', PLAYERS_PATH + '/' + id)
+            .then(() => {
+                sendRequest('GET', getUrlWithParamsPageSizeNumber())
+                    .then(players => {
+                        if (Object.keys(players).length > 0)
+                            updateTable(players)
+                        else
+                            location.reload()
+                    })
+                    .catch(() => console.log(NOT_FOUND_PLAYERS));
+            })
+            .catch(() => console.log(BAD_REQUEST))
+    }
+
 }
